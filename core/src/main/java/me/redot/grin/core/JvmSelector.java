85b8dac2bfc190ae615207d456277ae6b5c52bfe
Redot <masonjprice05@gmail.com>
2026-09-15 22:40:45 -0500

    chore: cleanup

--- core/src/main/java/me/redot/grin/core/JvmSelector.java
+++ core/src/main/java/me/redot/grin/core/JvmSelector.java
@@ -15,6 +15,7 @@ import java.io.PrintWriter;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 final class JvmSelector {
 
@@ -156,7 +157,7 @@ final class JvmSelector {
         }
     }
 
-    private static String styled(Terminal terminal, java.util.function.Consumer<AttributedStringBuilder> content) {
+    private static String styled(Terminal terminal, Consumer<AttributedStringBuilder> content) {
         AttributedStringBuilder builder = new AttributedStringBuilder();
         content.accept(builder);
         return builder.toAttributedString().toAnsi(terminal);
